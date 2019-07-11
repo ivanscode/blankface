301c052ee327d58b9711040dd1f835b9df8b9328
Erkam Uzun <euzun3@gatech.edu>
2019-07-11 18:56:16 -0400

    3d textureless sketch done. accuracy still worst

--- split.m
+++ split.m
@@ -8,13 +8,13 @@ nrof_big_img=floor(length(imgs)/(cnt^2));
 
 ind=1;
 for i=1:nrof_big_img
-    big_img=imread(['lfw_160_merged/m' int2str(i) '.jpg']);
+    big_img=imread(['lfw_nt_3d/m' int2str(i) '.jpg']);
     for j=1:cnt
         for k=1:cnt
             x=(j-1)*160+1:j*160;
             y=(k-1)*160+1:k*160;
             I=big_img(x,y,:);
-            imwrite(I,['lfw_160_splitted/' imgs{ind}]);
+            imwrite(I,['lfw_nt_3d_splitted/' imgs{ind}]);
             ind=ind+1;
         end
     end
@@ -23,13 +23,13 @@ end
 
 kCnt=11;
 jCnt=ceil((length(imgs)-nrof_big_img*(cnt^2))/kCnt);
-big_img=imread(['lfw_160_merged/m' int2str(i+1) '.jpg']);
+big_img=imread(['lfw_nt_3d/m' int2str(i+1) '.jpg']);
 for j=1:jCnt
     for k=1:kCnt
         x=(j-1)*160+1:j*160;
         y=(k-1)*160+1:k*160;
         I=big_img(x,y,:);
-            imwrite(I,['lfw_160_splitted/' imgs{ind}]);
+            imwrite(I,['lfw_nt_3d_splitted/' imgs{ind}]);
         ind=ind+1;
         if ind>length(imgs)
             break
